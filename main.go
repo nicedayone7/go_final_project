a17288f53d2b629ae7677283dec6c661f6bfa4d8
nicedayone7 <nicedayone7@gmail.com>
2024-06-13 10:50:35 +0000

    Fix fileserver add chi router for static files return

--- main.go
+++ main.go
@@ -2,25 +2,46 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 )
 
 const (
-	webDir = "../web"
-	serverAddr = "localhost:7540"
+	serverAddr = ":7540"
 )
 
-func handleMainPage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, ".webindex.html")
-	// http.ServeFile(w, r, "./web/js/")
+
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", http.StatusMovedPermanently).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request){
-		http.ServeFile(w, r, "web\\index.html")
-	})
-	// http.Handle("/", http.FileServer(http.Dir(webDir))) 
-	http.ListenAndServe(serverAddr, nil)
+	
+	fs := http.FileServer(http.Dir("web"))
+	r.Handle("/web/*", http.StripPrefix("/web/", fs))
+
+	workDir, _ := os.Getwd()
+	filesDir := http.Dir(filepath.Join(workDir, "web"))
+	FileServer(r, "/", filesDir)
+	
+	http.ListenAndServe(serverAddr, r)
 }
