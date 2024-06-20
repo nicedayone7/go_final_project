5c06ce9d6be4d74f0390c45dd190a94016f5550a
nicedayone7 <nicedayone7@gmail.com>
2024-06-20 09:25:07 +0000

    Restruct functions NextDate add file calc-date and normilize

--- calculate/calculate.go
+++ calculate/calculate.go
@@ -2,7 +2,6 @@ package calculate
 
 import (
 	"errors"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -10,20 +9,8 @@ import (
 const (
 	dateFormat = "20060102"
 )
-// mounths := map[int]string{
-// 	1:  time.January.String(),
-// 	2:  time.February.String(),
-// 	3:  time.March.String(),
-// 	4:  time.April.String(),
-// 	5:  time.May.String(),
-// 	6:  time.June.String(),
-// 	7:  time.July.String(),
-// 	8:  time.August.String(),
-// 	9:  time.September.String(),
-// 	10: time.October.String(),
-// 	11: time.November.String(),
-// 	12: time.December.String(),
-// }
+
+var ruleLettersTask = []string{"d", "y", "w", "m"}
 
 func NextDate(now time.Time, date string, repeat string) (string, error) {
 	startDate, err := time.Parse(dateFormat, date)
@@ -31,151 +18,69 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 		return "", nil
 	}
 	
-	role, daysRepeat, err := parseRepeat(repeat)
+	ruleLetter, err := parseRepeat(repeat)
 	if err != nil {
-		return "", nil
+		return "", err
 	}
 
 	switch {
-	case role == "d":
-		if len(daysRepeat) != 1 {
-			err := errors.New("does not match the rules format for days")
-			return "", err
-		}
-		nextDateTask, err := nextDayCalc(now, startDate, daysRepeat)
+	case ruleLetter == "d":
+		dayRepeat, err := normilizeDays(repeat)
 		if err != nil {
 			return "", err
 		}
-		resultDate := nextDateTask.Format(dateFormat)
-		if err != nil {
-			return "", nil
-		}
-		return resultDate, nil
-
-	
-	case role == "y":
-	 	if len(repeat) != 1 {
-			err := errors.New("does not match the rules format for year")
-			return "", err
-		}
-
-		if !startDate.After(now) {
-			nextYearTask := nextYearCalc(now, startDate)
-			return nextYearTask.Format(dateFormat), nil
-		}
-
-		return startDate.Format(dateFormat), nil
-
-	case role == "w":
-		if len(daysRepeat) < 1 || len(daysRepeat)  > 7 {
-			err := errors.New("does not match the rules format for week: lenght repeat rule more 7")
-				return "", err
-		}
-		addDay, err :=	nextDayWeek(daysRepeat, int(now.Weekday()))
+		
+		nextDateTask, err := nextDayCalc(now, startDate, dayRepeat)
 		if err != nil {
 			return "", err
 		}
-		nextWeekTask := now.AddDate(0,0,addDay)
+		
+		return nextDateTask.Format(dateFormat), nil
+
+	case ruleLetter == "y":
+	 	err := normilizeYear(repeat)
+		if err != nil {
+			return "", err
+		}
+
+		nextYearTask := nextYearCalc(now, startDate)
+		
+		return nextYearTask.Format(dateFormat), nil
+
+	case ruleLetter == "w":
+		daysWeek, err := normilizeDaysWeek(repeat)
+		if err != nil {
+			return "", err
+		}
+		
+		addDays, err :=	nextDayWeekCalc(daysWeek, int(now.Weekday()))
+		if err != nil {
+			return "", err
+		}
+		nextWeekTask := now.AddDate(0, 0, addDays)
 		return nextWeekTask.Format(dateFormat), nil
 		
-	case role == "m":
-
-		return "", nil	
-		
-
-	// 	if len()
-	// 	nextDay, err := nextDayWeek(strings.Split(days, ","), int(now.Weekday()))
-	// 	if err != nil {
-	// 		return "", err
-	// 	}
-		
-	// case role == "m":
-		
-		
-
-	}
-
-	return "", nil
-}
-
-func nextDayCalc(now time.Time, date time.Time, daysRepeat []int) (time.Time, error) {
-	dayRepeat := daysRepeat[0]
-	if dayRepeat >= 400 || dayRepeat <= 0 {
-		err := errors.New("the number must not exceed 400")
-		return time.Time{}, err
-	}
-
-	if dayRepeat == 1 {
-		return now, nil
-	}
-
-	diffDays := now.Sub(date).Hours()/24 // result different now and start task date
-	remainingDays := int(diffDays)%dayRepeat
-	
-	if remainingDays != 0 {
-		countRepeat := int(diffDays)/dayRepeat
-		dayToNextTask := countRepeat*(dayRepeat+1)
-		resultDate := date.AddDate(0, 0, dayToNextTask)
-		return resultDate, nil
-	}
-
-	return now, nil
-}
-
-func nextYearCalc(now time.Time, startDate time.Time) time.Time {
-	for !startDate.After(now) {
-		startDate = startDate.AddDate(1, 0, 0)
-	}
-	return startDate
-}
-
-
-func parseRepeat(dayRepeat string) (string, []int, error) {
-	var formatDays []int
-	role, days := strings.Split(dayRepeat, " ")[0], strings.Split(dayRepeat, " ")[1]
-	daysList := strings.Split(days, ",")
-	for _, day := range daysList {
-		day, err := strconv.Atoi(day)
+	case ruleLetter == "m":
+		parts := strings.Split(repeat, " ")
+		if len(parts) > 3 || len(parts) < 2 {
+			err := errors.New("invalid input data value")
+			return "", err
+		}
+		months, err := normilizeMonths(repeat)
 		if err != nil {
-			return "", nil, err
+			return "", err
 		}
-		formatDays = append(formatDays, day)
-	}
-	return role, formatDays, nil
-}
-
-func nextDayWeek(days []int, nowDay int) (int, error) {
-	for _, day := range days {
-		if day < 1 || day > 7 {
-			err := errors.New("does not match the rules format for week: day is incorrect out of range")
-			return 0, err
+		daysMonth, err := normilizeDaysForMonth(now, repeat)
+		if err != nil {
+			return "", err
 		}
-	}
-	
-
-	var result int
-	var previousDay int
-	if days[0] == nowDay {
-		return nowDay, nil
-	}
-
-	for i, day := range days {
-				
-		if nowDay > day {
-			result = 7 - nowDay + day
-		} else {
-			result = day - nowDay
+		nextMonthTask, err := nextDayMonth(now, repeat, parts, daysMonth, months)
+		if err != nil {
+			return "", err
 		}
-		
-		if i == 0 {
-			previousDay = result
-			continue
-		} 
-		
-		if previousDay > result {
-			previousDay = result
-		}
+		return nextMonthTask.Format(dateFormat), nil
+			
+	default:
+		return "", nil
 	}
-	
-	return result, nil
 }
---
+++ calculate/date-calc.go
@@ -0,0 +1,98 @@
+package calculate
+
+import (
+	"errors"
+	"strings"
+	"time"
+)
+
+func nextDayCalc(now time.Time, date time.Time, dayRepeat int) (time.Time, error) {
+	diffDays := now.Sub(date).Hours()/24 // result different now and start task date
+	remainingDays := int(diffDays)%dayRepeat
+	
+	if remainingDays != 0 {
+		countRepeat := int(diffDays)/dayRepeat
+		dayToNextTask := (countRepeat+1)*dayRepeat
+		resultDate := date.AddDate(0, 0, dayToNextTask)
+		return resultDate, nil
+	}
+
+	return now, nil
+}
+
+func nextYearCalc(now time.Time, startDate time.Time) time.Time {
+	for !startDate.After(now) {
+		startDate = startDate.AddDate(1, 0, 0)
+	}
+	return startDate
+}
+
+
+func parseRepeat(dayRepeat string) (string, error) {
+	ruleLetter := strings.Split(dayRepeat, " ")[0]
+	for _, rule := range ruleLettersTask {
+		if rule == ruleLetter {
+			return ruleLetter, nil
+		}
+	}
+	err := errors.New("invalid character")
+	return "", err
+}
+
+func nextDayWeekCalc(days []int, nowDay int) (int, error) {
+	var result int
+	var previousDay int
+
+	if days[0] == nowDay {
+		return 0, nil
+	}
+
+	for i, day := range days {		
+		if nowDay > day {
+			result = 7 - nowDay + day
+		} else {
+			result = day - nowDay
+		}
+		
+		if i == 0 {
+			previousDay = result
+			continue
+		} 
+		
+		if previousDay > result {
+			previousDay = result
+		}
+	}
+	
+	return result, nil
+}
+
+func nextDayMonth(now time.Time, rule string, parts []string, sortDays, sortMonths []int) (time.Time, error) {
+	if len(parts) == 2 {
+		for _, day := range sortDays {
+			if now.Day() < day {
+				nextDateTask := time.Date(now.Year(), now.Month(), day, 0,0,0,0,time.UTC)
+				return nextDateTask, nil
+			}
+		}
+		
+		nextDateTask := time.Date(now.Year(), now.Month()+1, sortDays[0], 0, 0, 0, 0, time.UTC)
+		return nextDateTask, nil
+	}
+
+	if len(parts) == 3 {
+		for _, month := range sortMonths {
+			for _, day := range sortDays {
+				findDate := time.Date(now.Year(), time.Month(month), day, 0, 0, 0, 0, time.UTC)
+				if findDate.After(now) {
+					return findDate, nil
+				}			
+			}
+		}
+		findDate := time.Date(now.Year()+1, time.Month(sortMonths[0]), sortDays[0], 0, 0, 0, 0, time.UTC)
+		return findDate, nil
+	}
+
+	err := errors.New("fail to find next date task")
+	return time.Time{}, err	
+}
